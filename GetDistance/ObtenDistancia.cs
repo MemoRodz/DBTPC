a9b54b87ffd0ad503dba39032cc7b6da58ac3157
MemoRodz <gmo.rodriguez@gmail.com>
2023-12-22 19:50:22 -0600

    Agregando proyecto de consola para verificación de funcionalidad de clases.

--- GetDistance/ObtenDistancia.cs
+++ GetDistance/ObtenDistancia.cs
@@ -4,14 +4,26 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-#region Referencia a Proyectos
 using distancePoints;
-#endregion
 
 namespace GetDistance
 {
     class ObtenDistancia
     {
-
+        static void Main(string[] args)
+        {
+            Console.WriteLine(distancePoints.getData.Saluda());
+            Console.WriteLine("Distancia: " + distancePoints.getData.ObtenDistancia("24645,GUSTAVO DIAZ ORDAZ,CALAKMUL,CAMPECHE", "24000,SAN FRANCISCO DE CAMPECHE CENTRO.,CAMPECHE,CAMPECHE", "MEXICO", "MEXICO").ToString());
+            Console.WriteLine("Distancia: " + distancePoints.getData.obtenDuracion("24645,GUSTAVO DIAZ ORDAZ,CALAKMUL,CAMPECHE", "24000,SAN FRANCISCO DE CAMPECHE CENTRO.,CAMPECHE,CAMPECHE", "MEXICO", "MEXICO"));
+            string distaancia = string.Empty;
+            string tieempo = string.Empty;
+            distancePoints.getData.obtenDuracionDistancia(
+                "24645,GUSTAVO DIAZ ORDAZ,CALAKMUL,CAMPECHE"
+                , "24000,SAN FRANCISCO DE CAMPECHE CENTRO,CAMPECHE,CAMPECHE"
+                , "MEXICO", "MEXICO", out distaancia, out tieempo
+                );
+            Console.WriteLine("Datos del recorrido, distancia: " + distaancia + " tiempo: " + tieempo);
+            Console.ReadKey();
+        }
     }
 }
