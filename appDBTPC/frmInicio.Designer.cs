d89e6166fa9b2141964279688e3c3c580c134cb2
MemoRodz <gmo.rodriguez@gmail.com>
2023-12-22 23:35:30 -0600

    Actualizando formato de celdas en tabla de plantilla.

--- appDBTPC/frmInicio.Designer.cs
+++ appDBTPC/frmInicio.Designer.cs
@@ -30,211 +30,215 @@ namespace appDBTPC
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmInicio));
             Mainmenu = new MenuStrip();
+            toolArchivo = new ToolStripMenuItem();
+            optionToolDirecto = new ToolStripMenuItem();
+            optionToolporArchivo = new ToolStripMenuItem();
+            optionToolporBase = new ToolStripMenuItem();
+            toolSeparador1 = new ToolStripSeparator();
+            optionToolSalir = new ToolStripMenuItem();
+            toolHerramientas = new ToolStripMenuItem();
+            optiontoolConfigurar = new ToolStripMenuItem();
+            optionToolPlantilla = new ToolStripMenuItem();
+            toolAyuda = new ToolStripMenuItem();
+            optionToolAcercade = new ToolStripMenuItem();
+            txtResult = new TextBox();
+            lblOrigen = new Label();
+            lblDestino = new Label();
+            txtOrigen = new TextBox();
+            txtDestino = new TextBox();
+            lblArchivo = new Label();
+            txtArchivo = new TextBox();
+            Mainmenu.SuspendLayout();
             SuspendLayout();
             // 
             // Mainmenu
             // 
-            this.Mainmenu.GripMargin = new System.Windows.Forms.Padding(2, 2, 0, 2);
-            this.Mainmenu.ImageScalingSize = new System.Drawing.Size(24, 24);
-            this.Mainmenu.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
-            this.toolArchivo,
-            this.toolHerramientas,
-            this.toolAyuda});
-            this.Mainmenu.Location = new System.Drawing.Point(0, 0);
-            this.Mainmenu.Name = "Mainmenu";
-            this.Mainmenu.Size = new System.Drawing.Size(819, 35);
-            this.Mainmenu.TabIndex = 0;
-            this.Mainmenu.Text = "Menú Principal";
+            Mainmenu.ImageScalingSize = new Size(24, 24);
+            Mainmenu.Items.AddRange(new ToolStripItem[] { toolArchivo, toolHerramientas, toolAyuda });
+            Mainmenu.Location = new Point(0, 0);
+            Mainmenu.Name = "Mainmenu";
+            Mainmenu.Padding = new Padding(2, 2, 0, 2);
+            Mainmenu.Size = new Size(819, 33);
+            Mainmenu.TabIndex = 0;
+            Mainmenu.Text = "Menú Principal";
             // 
             // toolArchivo
             // 
-            this.toolArchivo.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
-            this.optionToolDirecto,
-            this.optionToolporArchivo,
-            this.optionToolporBase,
-            this.toolSeparador1,
-            this.optionToolSalir});
-            this.toolArchivo.Name = "toolArchivo";
-            this.toolArchivo.Size = new System.Drawing.Size(103, 29);
-            this.toolArchivo.Text = "Opciones";
+            toolArchivo.DropDownItems.AddRange(new ToolStripItem[] { optionToolDirecto, optionToolporArchivo, optionToolporBase, toolSeparador1, optionToolSalir });
+            toolArchivo.Name = "toolArchivo";
+            toolArchivo.Size = new Size(103, 29);
+            toolArchivo.Text = "Opciones";
             // 
             // optionToolDirecto
             // 
-            this.optionToolDirecto.Name = "optionToolDirecto";
-            this.optionToolDirecto.Size = new System.Drawing.Size(205, 34);
-            this.optionToolDirecto.Text = "Directo";
-            this.optionToolDirecto.Click += new System.EventHandler(this.optionToolDirecto_Click);
+            optionToolDirecto.Name = "optionToolDirecto";
+            optionToolDirecto.Size = new Size(205, 34);
+            optionToolDirecto.Text = "Directo";
+            optionToolDirecto.Click += optionToolDirecto_Click;
             // 
             // optionToolporArchivo
             // 
-            this.optionToolporArchivo.Name = "optionToolporArchivo";
-            this.optionToolporArchivo.Size = new System.Drawing.Size(205, 34);
-            this.optionToolporArchivo.Text = "Por Archivo";
-            this.optionToolporArchivo.Click += new System.EventHandler(this.optionToolporArchivo_Click);
+            optionToolporArchivo.Name = "optionToolporArchivo";
+            optionToolporArchivo.Size = new Size(205, 34);
+            optionToolporArchivo.Text = "Por Archivo";
+            optionToolporArchivo.Click += optionToolporArchivo_Click;
             // 
             // optionToolporBase
             // 
-            this.optionToolporBase.Name = "optionToolporBase";
-            this.optionToolporBase.Size = new System.Drawing.Size(205, 34);
-            this.optionToolporBase.Text = "Por Base";
-            this.optionToolporBase.Click += new System.EventHandler(this.optionToolporBase_Click);
+            optionToolporBase.Name = "optionToolporBase";
+            optionToolporBase.Size = new Size(205, 34);
+            optionToolporBase.Text = "Por Base";
+            optionToolporBase.Click += optionToolporBase_Click;
             // 
             // toolSeparador1
             // 
-            this.toolSeparador1.Name = "toolSeparador1";
-            this.toolSeparador1.Size = new System.Drawing.Size(202, 6);
+            toolSeparador1.Name = "toolSeparador1";
+            toolSeparador1.Size = new Size(202, 6);
             // 
             // optionToolSalir
             // 
-            this.optionToolSalir.Name = "optionToolSalir";
-            this.optionToolSalir.Size = new System.Drawing.Size(205, 34);
-            this.optionToolSalir.Text = "&Salir";
-            this.optionToolSalir.Click += new System.EventHandler(this.optionToolSalir_Click);
+            optionToolSalir.Name = "optionToolSalir";
+            optionToolSalir.Size = new Size(205, 34);
+            optionToolSalir.Text = "&Salir";
+            optionToolSalir.Click += optionToolSalir_Click;
             // 
             // toolHerramientas
             // 
-            this.toolHerramientas.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
-            this.optiontoolConfigurar,
-            this.optionToolPlantilla});
-            this.toolHerramientas.Name = "toolHerramientas";
-            this.toolHerramientas.Size = new System.Drawing.Size(133, 29);
-            this.toolHerramientas.Text = "Herramientas";
+            toolHerramientas.DropDownItems.AddRange(new ToolStripItem[] { optiontoolConfigurar, optionToolPlantilla });
+            toolHerramientas.Name = "toolHerramientas";
+            toolHerramientas.Size = new Size(133, 29);
+            toolHerramientas.Text = "Herramientas";
             // 
             // optiontoolConfigurar
             // 
-            this.optiontoolConfigurar.Name = "optiontoolConfigurar";
-            this.optiontoolConfigurar.Size = new System.Drawing.Size(225, 34);
-            this.optiontoolConfigurar.Text = "Configuración";
+            optiontoolConfigurar.Name = "optiontoolConfigurar";
+            optiontoolConfigurar.Size = new Size(225, 34);
+            optiontoolConfigurar.Text = "Configuración";
             // 
             // optionToolPlantilla
             // 
-            this.optionToolPlantilla.Name = "optionToolPlantilla";
-            this.optionToolPlantilla.Size = new System.Drawing.Size(225, 34);
-            this.optionToolPlantilla.Text = "Plantilla";
-            this.optionToolPlantilla.Click += new System.EventHandler(this.optionToolPlantilla_Click);
+            optionToolPlantilla.Name = "optionToolPlantilla";
+            optionToolPlantilla.Size = new Size(225, 34);
+            optionToolPlantilla.Text = "Plantilla";
+            optionToolPlantilla.Click += optionToolPlantilla_Click;
             // 
             // toolAyuda
             // 
-            this.toolAyuda.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
-            this.optionToolAcercade});
-            this.toolAyuda.Name = "toolAyuda";
-            this.toolAyuda.Size = new System.Drawing.Size(79, 29);
-            this.toolAyuda.Text = "A&yuda";
+            toolAyuda.DropDownItems.AddRange(new ToolStripItem[] { optionToolAcercade });
+            toolAyuda.Name = "toolAyuda";
+            toolAyuda.Size = new Size(79, 29);
+            toolAyuda.Text = "A&yuda";
             // 
             // optionToolAcercade
             // 
-            this.optionToolAcercade.Name = "optionToolAcercade";
-            this.optionToolAcercade.Size = new System.Drawing.Size(203, 34);
-            this.optionToolAcercade.Text = "Acerca de...";
+            optionToolAcercade.Name = "optionToolAcercade";
+            optionToolAcercade.Size = new Size(203, 34);
+            optionToolAcercade.Text = "Acerca de...";
             // 
             // txtResult
             // 
-            this.txtResult.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
-            | System.Windows.Forms.AnchorStyles.Left)
-            | System.Windows.Forms.AnchorStyles.Right)));
-            this.txtResult.BackColor = System.Drawing.SystemColors.Window;
-            this.txtResult.Location = new System.Drawing.Point(18, 200);
-            this.txtResult.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
-            this.txtResult.Multiline = true;
-            this.txtResult.Name = "txtResult";
-            this.txtResult.ReadOnly = true;
-            this.txtResult.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
-            this.txtResult.Size = new System.Drawing.Size(781, 299);
-            this.txtResult.TabIndex = 8;
-            this.txtResult.Text = "\r\nPara un primer uso, favor de consultar la opción \"Acerca de\", del menú de \"Ayud" +
-    "a\".";
-            this.txtResult.TextChanged += new System.EventHandler(this.txtResult_TextChanged);
+            txtResult.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            txtResult.BackColor = SystemColors.Window;
+            txtResult.Location = new Point(18, 200);
+            txtResult.Margin = new Padding(4, 5, 4, 5);
+            txtResult.Multiline = true;
+            txtResult.Name = "txtResult";
+            txtResult.ReadOnly = true;
+            txtResult.ScrollBars = ScrollBars.Vertical;
+            txtResult.Size = new Size(781, 299);
+            txtResult.TabIndex = 8;
+            txtResult.Text = "\r\nPara un primer uso, favor de consultar la opción \"Acerca de\", del menú de \"Ayuda\".";
+            txtResult.TextChanged += txtResult_TextChanged;
             // 
             // lblOrigen
             // 
-            this.lblOrigen.AutoSize = true;
-            this.lblOrigen.Location = new System.Drawing.Point(18, 62);
-            this.lblOrigen.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
-            this.lblOrigen.Name = "lblOrigen";
-            this.lblOrigen.Size = new System.Drawing.Size(60, 20);
-            this.lblOrigen.TabIndex = 5;
-            this.lblOrigen.Text = "Origen:";
-            this.lblOrigen.Click += new System.EventHandler(this.lblOrigen_Click);
+            lblOrigen.AutoSize = true;
+            lblOrigen.Location = new Point(18, 62);
+            lblOrigen.Margin = new Padding(4, 0, 4, 0);
+            lblOrigen.Name = "lblOrigen";
+            lblOrigen.Size = new Size(70, 25);
+            lblOrigen.TabIndex = 5;
+            lblOrigen.Text = "Origen:";
+            lblOrigen.Click += lblOrigen_Click;
             // 
             // lblDestino
             // 
-            this.lblDestino.AutoSize = true;
-            this.lblDestino.Location = new System.Drawing.Point(18, 105);
-            this.lblDestino.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
-            this.lblDestino.Name = "lblDestino";
-            this.lblDestino.Size = new System.Drawing.Size(68, 20);
-            this.lblDestino.TabIndex = 6;
-            this.lblDestino.Text = "Destino:";
-            this.lblDestino.Click += new System.EventHandler(this.lblDestino_Click);
+            lblDestino.AutoSize = true;
+            lblDestino.Location = new Point(18, 105);
+            lblDestino.Margin = new Padding(4, 0, 4, 0);
+            lblDestino.Name = "lblDestino";
+            lblDestino.Size = new Size(68, 20);
+            lblDestino.TabIndex = 6;
+            lblDestino.Text = "Destino:";
+            lblDestino.Click += lblDestino_Click;
             // 
             // txtOrigen
             // 
-            this.txtOrigen.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left)
-            | System.Windows.Forms.AnchorStyles.Right)));
-            this.txtOrigen.Location = new System.Drawing.Point(86, 57);
-            this.txtOrigen.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
-            this.txtOrigen.Name = "txtOrigen";
-            this.txtOrigen.Size = new System.Drawing.Size(714, 26);
-            this.txtOrigen.TabIndex = 1;
-            this.txtOrigen.TextChanged += new System.EventHandler(this.txtOrigen_TextChanged);
-            this.txtOrigen.Enter += new System.EventHandler(this.txtOrigen_Enter);
+            txtOrigen.Anchor = AnchorStyles.Top | AnchorStyles.Left | AnchorStyles.Right;
+            txtOrigen.Location = new Point(86, 57);
+            txtOrigen.Margin = new Padding(4, 5, 4, 5);
+            txtOrigen.Name = "txtOrigen";
+            txtOrigen.Size = new Size(714, 26);
+            txtOrigen.TabIndex = 1;
+            txtOrigen.TextChanged += txtOrigen_TextChanged;
+            txtOrigen.Enter += txtOrigen_Enter;
             // 
             // txtDestino
             // 
-            this.txtDestino.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left)
-            | System.Windows.Forms.AnchorStyles.Right)));
-            this.txtDestino.Location = new System.Drawing.Point(86, 100);
-            this.txtDestino.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
-            this.txtDestino.Name = "txtDestino";
-            this.txtDestino.Size = new System.Drawing.Size(714, 26);
-            this.txtDestino.TabIndex = 2;
-            this.txtDestino.TextChanged += new System.EventHandler(this.txtDestino_TextChanged);
-            this.txtDestino.Enter += new System.EventHandler(this.txtDestino_Enter);
+            txtDestino.Anchor = AnchorStyles.Top | AnchorStyles.Left | AnchorStyles.Right;
+            txtDestino.Location = new Point(86, 100);
+            txtDestino.Margin = new Padding(4, 6, 4, 6);
+            txtDestino.Name = "txtDestino";
+            txtDestino.Size = new Size(714, 31);
+            txtDestino.TabIndex = 2;
+            txtDestino.TextChanged += txtDestino_TextChanged;
+            txtDestino.Enter += txtDestino_Enter;
             // 
             // lblArchivo
             // 
-            this.lblArchivo.AutoSize = true;
-            this.lblArchivo.Location = new System.Drawing.Point(15, 151);
-            this.lblArchivo.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
-            this.lblArchivo.Name = "lblArchivo";
-            this.lblArchivo.Size = new System.Drawing.Size(65, 20);
-            this.lblArchivo.TabIndex = 9;
-            this.lblArchivo.Text = "Archivo:";
-            this.lblArchivo.Click += new System.EventHandler(this.lblArchivo_Click);
+            lblArchivo.AutoSize = true;
+            lblArchivo.Location = new Point(15, 151);
+            lblArchivo.Margin = new Padding(4, 0, 4, 0);
+            lblArchivo.Name = "lblArchivo";
+            lblArchivo.Size = new Size(65, 20);
+            lblArchivo.TabIndex = 9;
+            lblArchivo.Text = "Archivo:";
+            lblArchivo.Click += lblArchivo_Click;
             // 
             // txtArchivo
             // 
-            this.txtArchivo.Location = new System.Drawing.Point(86, 146);
-            this.txtArchivo.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
-            this.txtArchivo.Name = "txtArchivo";
-            this.txtArchivo.Size = new System.Drawing.Size(714, 26);
-            this.txtArchivo.TabIndex = 10;
-            this.txtArchivo.TextChanged += new System.EventHandler(this.txtArchivo_TextChanged);
-            this.txtArchivo.Enter += new System.EventHandler(this.txtArchivo_Enter);
+            txtArchivo.Location = new Point(86, 146);
+            txtArchivo.Margin = new Padding(4, 5, 4, 5);
+            txtArchivo.Name = "txtArchivo";
+            txtArchivo.Size = new Size(714, 26);
+            txtArchivo.TabIndex = 10;
+            txtArchivo.TextChanged += txtArchivo_TextChanged;
+            txtArchivo.Enter += txtArchivo_Enter;
             // 
             // frmInicio
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(819, 520);
-            this.Controls.Add(this.txtArchivo);
-            this.Controls.Add(this.lblArchivo);
-            this.Controls.Add(this.txtDestino);
-            this.Controls.Add(this.txtOrigen);
-            this.Controls.Add(this.lblDestino);
-            this.Controls.Add(this.lblOrigen);
-            this.Controls.Add(this.txtResult);
-            this.Controls.Add(this.Mainmenu);
-            //this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
-            this.MainMenuStrip = this.Mainmenu;
-            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
-            this.Name = "frmInicio";
-            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
-            this.Text = "Obtener distancia de dos localidades";
-            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.frmInicio_KeyDown);
-            this.Mainmenu.ResumeLayout(false);
-            this.Mainmenu.PerformLayout();
+            AutoScaleDimensions = new SizeF(9F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(819, 520);
+            Controls.Add(txtArchivo);
+            Controls.Add(lblArchivo);
+            Controls.Add(txtDestino);
+            Controls.Add(txtOrigen);
+            Controls.Add(lblDestino);
+            Controls.Add(lblOrigen);
+            Controls.Add(txtResult);
+            Controls.Add(Mainmenu);
+            Icon = (Icon)resources.GetObject("$this.Icon");
+            MainMenuStrip = Mainmenu;
+            Margin = new Padding(4, 5, 4, 5);
+            Name = "frmInicio";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "Obtener distancia de dos localidades";
+            KeyDown += frmInicio_KeyDown;
+            Mainmenu.ResumeLayout(false);
+            Mainmenu.PerformLayout();
             ResumeLayout(false);
             PerformLayout();
         }
